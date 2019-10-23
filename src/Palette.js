46cc036dc9026cf621fe668ff6fc1f02ca07721f
Ola <olafrankowska112@gmail.com>
2019-10-23 11:00:05 +0200

    styled color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {level: 500};
+        this.state = { level: 500 };
         this.changeLevel = this.changeLevel.bind(this);
     }
-    changeLevel(level){
-        this.setState({level})
+    changeLevel(level) {
+        this.setState({ level })
     }
     render() {
-        const {colors} = this.props.palette;
-        const {level} = this.state;
+        const { colors } = this.props.palette;
+        const { level } = this.state;
         const colorBoxes = colors[level].map((c, i) => (
             <ColorBox background={c.hex} name={c.name} key={i} />
         ))
         return (
             <div className="Palette">
                 {/* Nav goes here */}
-                <Slider defaultValue={this.state.level} min={100} max={900}step={100} onAfterChange={this.changeLevel} />
+                <div className="slider">
+                    <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+                </div>
                 <div className="Palette-colors">
                     {/* color boxes */}
                     {colorBoxes}
