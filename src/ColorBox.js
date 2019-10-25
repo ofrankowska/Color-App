880973f914faf141cdf59a5e7aad40ca821b1c31
Ola <olafrankowska112@gmail.com>
2019-10-25 11:14:31 +0200

    adjusted text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 
 class ColorBox extends Component {
     constructor(props) {
@@ -17,25 +18,27 @@ class ColorBox extends Component {
     render() {
         const { name, background, colorId, paletteId, showLink } = this.props;
         const { copied } = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.25;
+        const isLightColor = chroma(background).luminance() >= 0.4;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className="ColorBox" style={{ background }}>
                     <div className={`copy-overlay ${copied && 'show'}`} style={{ background }}></div>
                     <div className={`copy-msg ${copied && 'show'}`}>
                         <h1>copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && 'dark-text'}>{background}</p>
                     </div>
                     <div className="copy-container" >
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && 'light-text'}>{name}</span>
                         </div>
 
-                        <span className="copy-button">COPY</span>
+                        <span className={`copy-button ${isLightColor && 'dark-text'}`}>COPY</span>
 
                     </div>
                     {showLink && (
                         <Link to={`/palette/${paletteId}/${colorId}`} onClick={(e) => e.stopPropagation()}>
-                            <span className="see-more">MORE</span>
+                            <span className={`see-more ${isLightColor && 'dark-text'}`}>MORE</span>
                         </Link>
                     )}
                 </div>
--- src/Palette.js
+++ src/Palette.js
@@ -21,7 +21,7 @@ class Palette extends Component {
         const { colors, paletteName, emoji, id } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(c => (
-            <ColorBox background={c[format]} name={c.name} key={c.id} colorId={c.id} paletteId={id} showLink />
+            <ColorBox background={c[format]} name={`${c.id} ${level}`} key={c.id} colorId={c.id} paletteId={id} showLink />
         ))
         return (
             <div className="Palette">
