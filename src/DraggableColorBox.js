04f93818ee486b99fbdab9ce63411a1e89d7d88d
Ola <olafrankowska112@gmail.com>
2019-10-29 17:01:31 +0100

    added delete to NewPaletteForm

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {withStyles} from '@material-ui/styles';
+import { withStyles } from '@material-ui/styles';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
 const styles = {
     root: {
@@ -11,17 +12,41 @@ const styles = {
         cursor: "pointer",
         textTransform: "uppercase",
         marginBottom: "-3.9px",
-
+        "&:hover svg": {
+            transform: "scale(1.5)",
+            color: "white"
+        }
+    },
+    boxContent: {
+        position: "absolute",
+        width: "90%",
+        bottom: "0px",
+        left: "0px",
+        padding: "10px",
+        color: "rgba(0, 0, 0, 0.5)",
+        letterSpacing: "1px",
+        fontSize: "12px",
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    deleteIcon: {
+        transition: "all 0.3s ease-in-out"
     }
 }
 
 function DraggableColorBox(props) {
+    const { classes, handleClick, name, color } = props;
     return (
         <div
-            className={props.classes.root}
-            style={{ backgroundColor: props.color }}
+            className={classes.root}
+            style={{ backgroundColor: color }}
         >
-            {props.name}
+            <div className={classes.boxContent}>
+                <span>{name}</span>
+                <DeleteOutlinedIcon className={classes.deleteIcon}
+                    onClick={handleClick}
+                />
+            </div>
         </div>
     )
 }
