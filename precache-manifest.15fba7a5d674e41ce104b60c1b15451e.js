8f2f37cf5139c30d6e447da89b0e5c059317fa57
Ola <olafrankowska112@gmail.com>
2019-11-06 13:11:20 +0100

    Updates

---
+++ precache-manifest.15fba7a5d674e41ce104b60c1b15451e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958ef46fcf0137fbca8c550aab14f29e",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "6cb50016a90d26820de2",
+    "url": "/react-colors/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "a21ae6338a2e0cce47bd",
+    "url": "/react-colors/static/css/main.2f78f16b.chunk.css"
+  },
+  {
+    "revision": "6cb50016a90d26820de2",
+    "url": "/react-colors/static/js/2.086df422.chunk.js"
+  },
+  {
+    "revision": "a21ae6338a2e0cce47bd",
+    "url": "/react-colors/static/js/main.4ecbf4fc.chunk.js"
+  },
+  {
+    "revision": "1533b2cf556394fd5c12",
+    "url": "/react-colors/static/js/runtime-main.55bc9266.js"
+  },
+  {
+    "revision": "e2d26d8439068766048449e98e9555b5",
+    "url": "/react-colors/static/media/Confetti-Doodles.e2d26d84.svg"
+  }
+]);
